1211fa17e106a4b243cecd0020ac347331f3bc9e
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-08-15 15:38:55 +0300

    fixup! Add type of the object to the index file of che-plugin-registry

--- index.sh
+++ index.sh
@@ -13,14 +13,21 @@ do
     else
         echo ",{"
     fi
+
     # 1. read meta.yaml to stio
-    # 2. filter lines with name,version,type
-    # 3. Replace ` :` with `":"`
-    # 4. Append `",` to the end of each line
-    # 5. Append `"` at the begining of each line
-    # 6. Remove all new lines
-    # 7. Remove last ','
-    cat $i|grep -e name -e version -e type |   sed 's/: /\":"/g'  | sed 's/$/\",/g' | sed 's/^/\"/g'  | tr -d '\n' |  sed 's/,$//g'
+    cat $i| \
+        # 2. filter lines with name,version,type
+        grep -e name -e version -e type |\
+        # 3. Replace ` :` with `":"`
+        sed 's/: /\":"/g'  |\
+        # 4. Append `",` to the end of each line
+        sed 's/$/\",/g' |\
+        # 5. Append `"` at the beginning of each line
+        sed 's/^/\"/g'  |\
+        # 6. Remove all new lines
+        tr -d '\n' |\
+        # 7. Remove last ','
+        sed 's/,$//g'
     echo "}"
 done
 echo "]"
