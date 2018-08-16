22c27628f2db4bfbcccdd70e9ee78c1f33d4cfdf
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2018-08-16 11:13:56 +0300

    Add type of the object to the index file of che-plugin-registry (#6)
    Add type of the object to the index file of che-plugin-registry (#6)

--- index.sh
+++ index.sh
@@ -7,12 +7,27 @@ echo "["
 ## now loop through meta files
 for i in "${arr[@]}"
 do
-    IFS='/' segments=($i)
     if [ "$FIRST_LINE" = true ] ; then
-        echo \{\"name\":\"${segments[2]}\",\"version\":\"${segments[3]}\"\}
+        echo "{"
         FIRST_LINE=false
     else
-        echo \,\{\"name\":\"${segments[2]}\",\"version\":\"${segments[3]}\"\}
+        echo ",{"
     fi
+
+    # 1. read meta.yaml to stio
+    cat $i| \
+        # 2. filter lines with name,version,type
+        grep -e name -e version -e type |\
+        # 3. Replace ` :` with `":"`
+        sed 's/: /\":"/g'  |\
+        # 4. Append `",` to the end of each line
+        sed 's/$/\",/g' |\
+        # 5. Append `"` at the beginning of each line
+        sed 's/^/\"/g'  |\
+        # 6. Remove all new lines
+        tr -d '\n' |\
+        # 7. Remove last ','
+        sed 's/,$//g'
+    echo "}"
 done
 echo "]"
