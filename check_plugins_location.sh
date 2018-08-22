200bfc958e36c3e55169ff8ad39a3d5531ce7422
Mykhailo Kuznietsov <mkuznets@redhat.com>
2018-08-22 18:13:24 +0300

    Add script to check plugins version and id

---
+++ check_plugins_location.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+## browse all plugin directories which names goes as follow
+##  <plugin id>/<plugin version>
+## and check that directory names match with id/version in meta.yaml files
+cd plugins
+for d in */ ; do
+  ID_DIR_NAME=${d%/}
+  cd $d
+
+  VERSION_DIR_NAME=$(ls -d */)
+  VERSION_DIR_NAME=${VERSION_DIR_NAME%/}
+  cd $VERSION_DIR_NAME
+
+
+  ID_YAML=$(grep "id: " meta.yaml)
+  ID_YAML=${ID_YAML:4}
+  if [[ "$ID_YAML" != "$ID_DIR_NAME" ]];then
+    echo "!!! ID mismatch in plugin '${ID_DIR_NAME}/${VERSION_DIR_NAME}':"
+    echo "!!!   id in meta.yaml: '${ID_YAML}'"
+    echo "!!!   id directory name: '${ID_DIR_NAME}' "
+    FOUND=true
+  fi
+
+  VERSION_YAML=$(grep "version: " meta.yaml)
+  VERSION_YAML=${VERSION_YAML:9}
+  if [[ "$VERSION_YAML" != "$VERSION_DIR_NAME" ]];then
+    echo "!!! Version mismatch in plugin '${ID_DIR_NAME}/${VERSION_DIR_NAME}':"
+    echo "!!!   version in meta.yaml: '${VERSION_YAML}'"
+    echo "!!!   version directory name: '${VERSION_DIR_NAME}' "
+    FOUND=true
+  fi
+
+  cd ../..
+done
+
+if [[ $FOUND ]];then
+  exit 1
+fi
