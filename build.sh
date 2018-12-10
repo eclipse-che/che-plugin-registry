c9090979d2d151224b3bdd1996800f7125dde782
Max Shaposhnik <mshaposh@redhat.com>
2018-12-10 13:34:02 +0200

    Add licenses for the scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+#
+# Copyright (c) 2012-2018 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
 
 docker build -t eclipse/che-plugin-registry:latest .
