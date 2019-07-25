194a553e6a295d746484a044112adb553cff8a51
Mario Loriedo <mario.loriedo@gmail.com>
2019-07-25 19:33:33 +0200

    Remove duplicate code
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- cico_build_ci.sh
+++ cico_build_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012-2018 Red Hat, Inc.
+# Copyright (c) 2012-2019 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -14,83 +14,13 @@ set -x
 # Exit on error
 set -e
 
-# Source environment variables of the jenkins slave
-# that might interest this worker.
-function load_jenkins_vars() {
-  if [ -e "jenkins-env.json" ]; then
-    eval "$(./env-toolkit load -f jenkins-env.json \
-            DEVSHIFT_TAG_LEN \
-            QUAY_USERNAME \
-            QUAY_PASSWORD \
-            QUAY_ECLIPSE_CHE_USERNAME \
-            QUAY_ECLIPSE_CHE_PASSWORD \
-            JENKINS_URL \
-            GIT_BRANCH \
-            GIT_COMMIT \
-            BUILD_NUMBER \
-            ghprbSourceBranch \
-            ghprbActualCommit \
-            BUILD_URL \
-            ghprbPullId)"
-  fi
-}
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+export SCRIPT_DIR
 
-function install_deps() {
-  # We need to disable selinux for now, XXX
-  /usr/sbin/setenforce 0  || true
+# shellcheck disable=SC1090
+. "${SCRIPT_DIR}"/cico_functions.sh
 
-  # Get all the deps in
-  yum install -y yum-utils device-mapper-persistent-data lvm2
-  yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  yum install -y docker-ce \
-    git
-
-  service docker start
-
-  echo 'CICO: Dependencies installed'
-}
-
-function tag_push() {
-  local TARGET=$1
-  docker tag "${IMAGE}" "$TARGET"
-  docker push "$TARGET"
-}
-
-function deploy() {
-  TARGET=${TARGET:-"centos"}
-  REGISTRY="quay.io"
-
-  if [ "$TARGET" == "rhel" ]; then
-    DOCKERFILE="Dockerfile.rhel"
-    ORGANIZATION="openshiftio"
-    IMAGE="rhel-che-plugin-registry"
-  else
-    DOCKERFILE="Dockerfile"
-    ORGANIZATION="eclipse"
-    IMAGE="che-plugin-registry"
-    # For pushing to quay.io 'eclipse' organization we need to use different credentials
-    QUAY_USERNAME=${QUAY_ECLIPSE_CHE_USERNAME}
-    QUAY_PASSWORD=${QUAY_ECLIPSE_CHE_PASSWORD}
-  fi
-
-  if [ -n "${QUAY_USERNAME}" ] && [ -n "${QUAY_PASSWORD}" ]; then
-    docker login -u "${QUAY_USERNAME}" -p "${QUAY_PASSWORD}" "${REGISTRY}"
-  else
-    echo "Could not login, missing credentials for the registry"
-  fi
-
-  # Let's deploy
-  docker build -t ${IMAGE} -f ${DOCKERFILE} .
-
-  TAG=$(echo "$GIT_COMMIT" | cut -c1-"${DEVSHIFT_TAG_LEN}")
-
-  tag_push "${REGISTRY}/${ORGANIZATION}/$IMAGE:$TAG"
-  echo "CICO: Image pushed to '${REGISTRY}/${ORGANIZATION}', ready to update deployed app"
-}
-
-function cico_setup() {
-  load_jenkins_vars;
-  install_deps;
-}
-cico_setup
-deploy
+load_jenkins_vars
+install_deps
+set_ci_tag
+build_and_push
