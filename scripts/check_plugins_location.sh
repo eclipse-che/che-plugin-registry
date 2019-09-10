9b24da2aa56b9232f2a37fbab9e9d6586be796d6
nickboldt <nboldt@redhat.com>
2019-09-10 14:14:48 -0400

    to make it easier to run the check*.sh scripts locally, use dirname \bash instead of . as path to util.sh
    Change-Id: I08ac3380ba9f2a8ea3489a4dc77befe052f0cf84
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- scripts/check_plugins_location.sh
+++ scripts/check_plugins_location.sh
@@ -15,7 +15,7 @@
 set -e
 
 # shellcheck source=./scripts/util.sh
-source ./util.sh
+source "$(dirname "$0")/util.sh"
 
 readarray -d '' metas < <(find "$1" -name 'meta.yaml' -print0)
 for meta in "${metas[@]}"; do
--- scripts/check_plugins_viewer_mandatory_fields.sh
+++ scripts/check_plugins_viewer_mandatory_fields.sh
@@ -12,7 +12,7 @@
 set -e
 
 # shellcheck source=./scripts/util.sh
-source ./util.sh
+source "$(dirname "$0")/util.sh"
 
 readarray -d '' metas < <(find "$1" -name 'meta.yaml' -print0)
 
