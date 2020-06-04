286393285217123d50c3b8c1b6bcb8b873d454dc
Ihor Okhrimenko <iokhrime@redhat.com>
2020-06-04 16:03:09 +0300

    Next try #3
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/cico-happy-path-pr-test.sh
+++ .ci/cico-happy-path-pr-test.sh
@@ -14,15 +14,6 @@ set -x
 
 
 export IS_TESTS_FAILED="false"
-CHE_SERVER_PATCH="$(cat <<EOL
-spec:
-  server:
-    pluginRegistryImage: $IMAGE_NAME
-    selfSignedCert: true
-  auth:
-    updateAdminPassword: false
-EOL
-)"
 
 #Download and import the "common-qe" functions
 DOWNLOADER_URL=https://raw.githubusercontent.com/eclipse/che/iokhrime-common-centos/.ci/common-qe/downloader.sh
@@ -34,6 +25,15 @@ setup_environment
 
 export TAG="PR-${ghprbPullId:?}"
 export IMAGE_NAME="quay.io/eclipse/che-plugin-registry:$TAG"
+CHE_SERVER_PATCH="$(cat <<EOL
+spec:
+  server:
+    pluginRegistryImage: $IMAGE_NAME
+    selfSignedCert: true
+  auth:
+    updateAdminPassword: false
+EOL
+)"
 
 buildAndPushRepoDockerImage "$TAG"
 
