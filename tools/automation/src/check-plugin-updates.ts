09c476b07861057bb2d2839b1d95d95fb8014440
Eric Williams <ericwill@redhat.com>
2020-08-11 14:20:09 -0400

    Sidecar update checking (#558)
    * Sidecar update checking
    * Use regex instead of split
    
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- tools/automation/src/check-plugin-updates.ts
+++ tools/automation/src/check-plugin-updates.ts
@@ -16,23 +16,37 @@ import * as semver from 'semver';
 
 import simpleGit, { SimpleGit } from 'simple-git';
 
-const ROOT_DIR = '/tmp/repository';
+const EXTENSION_ROOT_DIR = '/tmp/extension_repository';
+const SIDECAR_ROOT_DIR = '/tmp/sidecar_repository';
 
 // struct for the array of vscode-extensions.json file
 export interface Extension {
   repository: string;
   revision: string;
   directory?: string;
+  sidecar?: {
+    image: string;
+    source: {
+      repository: string;
+      revision?: string;
+      directory?: string;
+    };
+  };
 }
 
 // Entry generated when inspecting an extension
 export interface Entry {
   repositoryName: string;
   clonePath: string;
+  sidecarClonePath?: string;
   extensionName?: string;
   registryVersion?: string;
   upstreamVersion?: string;
-  needsUpdating?: boolean;
+  extensionNeedsUpdating?: boolean;
+  registrySidecarImage?: string;
+  upstreamSidecarImage?: string;
+  sidecarLocation?: string;
+  sidecarNeedsUpdating?: boolean;
   errors: string[];
 }
 
@@ -52,17 +66,18 @@ export class Report {
     const start = moment();
     const { extensions } = JSON.parse(await fs.readFile('./../../vscode-extensions.json', 'utf-8'));
 
-    // cleanup folder
-    await fs.remove(ROOT_DIR);
+    // cleanup folders
+    await fs.remove(EXTENSION_ROOT_DIR);
+    await fs.remove(SIDECAR_ROOT_DIR);
 
-    // grab result in parallel
+    // grab results in parallel
     const entries: Entry[] = await Promise.all(
       extensions.map(
         async (extension: Extension): Promise<Entry> => {
           // path where to clone extension (remove all invalid characters)
           const cloneName = extension.repository.replace(/[^\w\s]/gi, '');
           const entry: Entry = {
-            clonePath: path.resolve(ROOT_DIR, cloneName),
+            clonePath: path.resolve(EXTENSION_ROOT_DIR, cloneName),
             repositoryName: extension.repository,
             errors: [],
           };
@@ -95,17 +110,68 @@ export class Report {
           packageJSON = JSON.parse(await fs.readFile(packageJSONPath, 'utf-8'));
           entry.registryVersion = packageJSON.version;
           if (!entry.upstreamVersion) {
-            entry.needsUpdating = false;
+            entry.extensionNeedsUpdating = false;
             return this.handleError(entry, `Failure: there is no upstream version`);
           } else if (!entry.registryVersion) {
-            entry.needsUpdating = false;
+            entry.extensionNeedsUpdating = false;
             return this.handleError(entry, `Failure: there is no registry version`);
           } else {
-            entry.needsUpdating = semver.gt(entry.upstreamVersion, entry.registryVersion);
+            entry.extensionNeedsUpdating = semver.gt(entry.upstreamVersion, entry.registryVersion);
           }
 
           // cleanup cloned directory for this extension
           await fs.remove(entry.clonePath);
+
+          // If the extension has a sidecar image, let's check it too
+          if (extension.sidecar) {
+            const sidecarCloneName = extension.sidecar.source.repository.replace(/[^\w\s]/gi, '');
+            // Create unique clone path for the sidecar, otherwise there may be collisions
+            entry.sidecarClonePath = path.resolve(SIDECAR_ROOT_DIR, sidecarCloneName, entry.clonePath);
+
+            await fs.remove(entry.sidecarClonePath);
+            // Clone sidecar repository
+            try {
+              await git.clone(extension.sidecar.source.repository, entry.sidecarClonePath);
+            } catch (err) {
+              return this.handleError(entry, `Error cloning sidecar repository: ${err}`);
+            }
+
+            const sidecarNameSplit = extension.sidecar.image.split(':');
+            entry.sidecarLocation = `https://${sidecarNameSplit[0]}`;
+            const registryImageVersion = sidecarNameSplit[1];
+            // Filter out cases that do not adhere to the sidecar naming scheme
+            if (
+              registryImageVersion &&
+              (!registryImageVersion.includes('latest') || !registryImageVersion.includes('next'))
+            ) {
+              let sidecarSHA1;
+              entry.registrySidecarImage = registryImageVersion;
+              // Run git rev-parse --short HEAD and get hash of the upstream image
+              try {
+                const gitRevision = extension.sidecar.source.revision
+                  ? `origin/${extension.sidecar.source.revision}`
+                  : 'HEAD';
+                sidecarSHA1 = await simpleGit(entry.sidecarClonePath).revparse(['--short', `${gitRevision}`]);
+              } catch (err) {
+                return this.handleError(entry, `Error executing git rev-parse in sidecar repository: ${err}`);
+              }
+              // Compare versions
+              let upstreamSidecarImageVersion;
+              if (extension.sidecar.source.revision) {
+                upstreamSidecarImageVersion = `${extension.sidecar.source.revision}-${sidecarSHA1}`;
+                entry.sidecarNeedsUpdating = registryImageVersion !== upstreamSidecarImageVersion;
+              } else {
+                const sidecarRepoVersion = await fs.readFile(path.join(entry.sidecarClonePath, 'VERSION'), 'utf-8');
+                const sidecarVersion = sidecarRepoVersion.replace(/\n/gi, '');
+                upstreamSidecarImageVersion = `${sidecarVersion}-${sidecarSHA1}`;
+              }
+              entry.upstreamSidecarImage = upstreamSidecarImageVersion;
+              entry.sidecarNeedsUpdating = registryImageVersion !== upstreamSidecarImageVersion;
+            } else {
+              return this.handleError(entry, `Error checking sidecar image versions: image name format not supported.`);
+            }
+            await fs.remove(entry.sidecarClonePath);
+          }
 
           console.log(`✅ ${extension.repository}`);
           return entry;
@@ -137,8 +203,9 @@ export class Report {
       console.log(`Failed to write the report file (./report/index.md)`);
     }
 
-    // cleanup root cloned path
-    await fs.remove(ROOT_DIR);
+    // cleanup root cloned paths
+    await fs.remove(EXTENSION_ROOT_DIR);
+    await fs.remove(SIDECAR_ROOT_DIR);
 
     return;
   }
