6c25275dac3a3bb51236214373b44844c28fd65f
Eric Williams <ericwill@redhat.com>
2020-11-03 16:10:39 -0500

    Fix dead code, disable SC2199
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- .ci/sidecar-build-pr-check.sh
+++ .ci/sidecar-build-pr-check.sh
@@ -19,22 +19,20 @@ for file in "${FILES_CHANGED[@]}"
 do
     if [[ $file == sidecars/* ]]; then
         SIDECAR_NAME=$(echo "$file" | cut -d/ -f 2)
-        for sidecar in "${SIDECARS_TO_TEST[@]}"
-        do
-            if ! [[ "$sidecar" =~ ${SIDECAR_NAME} ]]; then
-                SIDECARS_TO_TEST+=("$SIDECAR_NAME")
-                PLATFORMS=$(cat sidecars/"$SIDECAR_NAME"/PLATFORMS)
-                SHORT_SHA1=$(git rev-parse --short HEAD)
-                if [[ $BUILD_CHECK == 'build' ]]; then
-                    echo "Building quay.io/eclipse/che-plugin-sidecar:$SIDECAR_NAME-$SHORT_SHA1"
-                    docker buildx build --platform "$PLATFORMS" -t quay.io/eclipse/che-plugin-sidecar:"$SIDECAR_NAME"-"$SHORT_SHA1" \
-                        --push sidecars/"$SIDECAR_NAME"/
-                fi
-                if [[ $BUILD_CHECK == 'check' ]]; then
-                    echo "Checking $SIDECAR_NAME-$SHORT_SHA1"
-                    docker buildx build --platform "$PLATFORMS" sidecars/"$SIDECAR_NAME"/
-                fi
+        # shellcheck disable=SC2199
+        if ! [[ " ${SIDECARS_TO_TEST[@]} " =~ ${SIDECAR_NAME} ]]; then
+            SIDECARS_TO_TEST+=("$SIDECAR_NAME")
+            PLATFORMS=$(cat sidecars/"$SIDECAR_NAME"/PLATFORMS)
+            SHORT_SHA1=$(git rev-parse --short HEAD)
+            if [[ $BUILD_CHECK == 'build' ]]; then
+                echo "Building quay.io/eclipse/che-plugin-sidecar:$SIDECAR_NAME-$SHORT_SHA1"
+                docker buildx build --platform "$PLATFORMS" -t quay.io/eclipse/che-plugin-sidecar:"$SIDECAR_NAME"-"$SHORT_SHA1" \
+                    --push sidecars/"$SIDECAR_NAME"/
             fi
-        done
+            if [[ $BUILD_CHECK == 'check' ]]; then
+                echo "Checking $SIDECAR_NAME-$SHORT_SHA1"
+                docker buildx build --platform "$PLATFORMS" sidecars/"$SIDECAR_NAME"/
+            fi
+        fi
     fi
 done
