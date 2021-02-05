1ad5e554ab6445cd8e253aded5b198aeaba3478c
svor <vsvydenk@redhat.com>
2021-02-05 23:18:15 +0200

    update action
    Signed-off-by: svor <vsvydenk@redhat.com>

--- .ci/prepare-extension-project.sh
+++ .ci/prepare-extension-project.sh
@@ -60,16 +60,16 @@ function createWorkspace() {
     echo "$THEIA_IDE_CONTAINER_NAME" 
 
     # Start the python3 selenium script that will connect to the workspace to run tests
-    python3 .ci/language-tests-runner.py "${WORKSPACE_URL}"
-    sleep 20
-    cat geckodriver.log
+    # python3 .ci/language-tests-runner.py "${WORKSPACE_URL}"
+    # sleep 20
+    # cat geckodriver.log
 
-    ### Copy extension's sources into theia container
-    kubectl cp /tmp/projects/$YAML_EXTENSION_PROJECT_NAME eclipse-che/"${WORKSPACE_NAME}":/pojects -c $THEIA_IDE_CONTAINER_NAME    
-    ### Check if copy
-    kubectl cp eclipse-che/"${WORKSPACE_NAME}":/pojects/vscode-yaml/package.json /tmp/package.json -c $THEIA_IDE_CONTAINER_NAME    
-    echo "----- Check content of package.json --------"
-    cat /tmp/package.json
+    # ### Copy extension's sources into theia container
+    # kubectl cp /tmp/projects/$YAML_EXTENSION_PROJECT_NAME eclipse-che/"${WORKSPACE_NAME}":/pojects -c $THEIA_IDE_CONTAINER_NAME    
+    # ### Check if copy
+    # kubectl cp eclipse-che/"${WORKSPACE_NAME}":/pojects/vscode-yaml/package.json /tmp/package.json -c $THEIA_IDE_CONTAINER_NAME    
+    # echo "----- Check content of package.json --------"
+    # cat /tmp/package.json
 }
 
 findRepositoryDetails
