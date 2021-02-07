759e3598ea033a6b89f59702eac1ed79a92acda4
svor <vsvydenk@redhat.com>
2021-02-07 18:33:22 +0200

    update action
    Signed-off-by: svor <vsvydenk@redhat.com>

--- .ci/language-tests-runner.py
+++ .ci/language-tests-runner.py
@@ -26,7 +26,7 @@ options.add_argument('--ignore-certificate-errors')
 
 NEW_USER="admin"
 
-browser = webdriver.Firefox(options=options, executable_path="/usr/local/bin/geckodriver")
+browser = webdriver.Firefox(options=options, executable_path="/usr/local/bin/geckodriver", log_path='/tmp/selenium.log')
 wait = WebDriverWait(browser, 30)
 print ("URL is ------------------>")
 print (sys.argv[1])
@@ -47,5 +47,5 @@ browser.implicitly_wait(20)
 browser.get(sys.argv[1])
 
 #Waiting for theia itself to be loaded so that tests will be run
-# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'ide-application-iframe')))
-# wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="theia-app-shell"]')))
+wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'ide-application-iframe')))
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="theia-app-shell"]')))
