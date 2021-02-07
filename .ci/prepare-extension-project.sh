5a5e50cbed2f990177d31c0b4f013eede9a3a601
svor <vsvydenk@redhat.com>
2021-02-07 16:24:37 +0200

    update action
    Signed-off-by: svor <vsvydenk@redhat.com>

--- .ci/prepare-extension-project.sh
+++ .ci/prepare-extension-project.sh
@@ -62,7 +62,7 @@ function createWorkspace() {
     # Start the python3 selenium script that will connect to the workspace to run tests
     python3 $GITHUB_WORKSPACE/.ci/language-tests-runner.py "${WORKSPACE_URL}"
     sleep 20
-    cat geckodriver.log
+    cat $GITHUB_WORKSPACE/.ci/geckodriver.log
 
     # ### Copy extension's sources into theia container
     # kubectl cp /tmp/projects/$YAML_EXTENSION_PROJECT_NAME eclipse-che/"${WORKSPACE_NAME}":/pojects -c $THEIA_IDE_CONTAINER_NAME    
