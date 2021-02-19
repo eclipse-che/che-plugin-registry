f0f629cc496b1083abefc13b844581e544cd8eb6
svor <vsvydenk@redhat.com>
2021-02-19 12:01:35 +0200

    update
    Signed-off-by: svor <vsvydenk@redhat.com>

--- .ci/prepare-extension-project.sh
+++ .ci/prepare-extension-project.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Copyright (c) 2021 Red Hat, Inc.
+# shellcheck disable=SC1091,SC1090
+#
+# Copyright (c) 2012-2021 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
