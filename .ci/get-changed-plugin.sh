a5f81fef28a88106792af4e9ea0d37857ff6fa2b
svor <vsvydenk@redhat.com>
2021-02-22 15:57:41 +0200

    update
    Signed-off-by: svor <vsvydenk@redhat.com>

---
+++ .ci/get-changed-plugin.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# shellcheck disable=SC2016
+#
+# Copyright (c) 2012-2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+set -e
+
+sudo pip install yq
+
+function getExtensionRepoAndRevision() {
+    CHANGED_LINES=$(git diff -U0 HEAD "$(git merge-base HEAD origin/master)" che-theia-plugins.yaml | grep @@ | cut -d ' ' -f 3 | sed 's/+//')
+    echo "$CHANGED_LINES"
+    for number in $CHANGED_LINES
+    do
+        LINE=$(sed "$number!d" che-theia-plugins.yaml | xargs)
+        echo --- LINE --- 
+        echo "$LINE"
+        if [[ $LINE == *"revision:"* ]]; then
+            break
+        fi
+        exit 0
+    done
+    EXTENSION_REVISION=$(echo "$LINE" | cut -d ' ' -f 2)
+    EXTENSION_REPO=$(yq -r --arg EXTENSION_REVISION "$EXTENSION_REVISION" '[.plugins[] | select(.repository.revision == $EXTENSION_REVISION )] | .[0] | .repository.url' che-theia-plugins.yaml)
+    export EXTENSION_REPO
+    export EXTENSION_REVISION
+
+    echo --- REPOSITORY ---
+    echo Extension repo is "$EXTENSION_REPO"
+    echo --- REVISION ---
+    echo Extension revision is "$EXTENSION_REVISION"
+    if [ "$EXTENSION_REPO" == null ];
+    then
+        exit 0
+    fi
+}
+
+getExtensionRepoAndRevision
--- .ci/prepare-extension-project.sh
+++ .ci/prepare-extension-project.sh
@@ -14,40 +14,11 @@
 set -e
 
 function installDeps() {
-    sudo pip install yq
     sudo pip install selenium
     wget https://github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
     tar -xvzf geckodriver*
     chmod +x geckodriver
     sudo mv geckodriver /usr/local/bin/
-}
-
-function getExtensionRepo() {
-    CHANGED_LINES=$(git diff -U0 HEAD "$(git merge-base HEAD origin/master)" che-theia-plugins.yaml | grep @@ | cut -d ' ' -f 3 | sed 's/+//')
-    echo "$CHANGED_LINES"
-    for number in $CHANGED_LINES
-    do
-        LINE=$(sed "$number!d" che-theia-plugins.yaml | xargs)
-        echo --- LINE --- 
-        echo "$LINE"
-        if [[ $LINE == *"revision:"* ]]; then
-            break
-        fi
-        exit 0
-    done
-    EXTENSION_REVISION=$(echo "$LINE" | cut -d ' ' -f 2)
-    EXTENSION_REPO=$(yq -r --arg EXTENSION_REVISION "$EXTENSION_REVISION" '[.plugins[] | select(.repository.revision == $EXTENSION_REVISION )] | .[0] | .repository.url' che-theia-plugins.yaml)
-    export EXTENSION_REPO
-    export EXTENSION_REVISION
-
-    echo --- REPOSITORY ---
-    echo Extension repo is $EXTENSION_REPO
-    echo --- REVISION ---
-    echo Extension revision is $EXTENSION_REVISION
-    if [ "$EXTENSION_REPO" == null ];
-    then
-        exit 0
-    fi
 }
 
 function cloneExtension() {
