0a3dc8f828fbd978e57552a6666096bdb2424340
svor <vsvydenk@redhat.com>
2021-02-23 11:23:26 +0200

    update
    Signed-off-by: svor <vsvydenk@redhat.com>

--- .ci/get-changed-plugin.sh
+++ .ci/get-changed-plugin.sh
@@ -15,30 +15,24 @@ set -e
 
 sudo pip install yq
 
-function getExtensionRepoAndRevision() {
-    CHANGED_LINES=$(git diff -U0 HEAD "$(git merge-base HEAD origin/master)" che-theia-plugins.yaml | grep @@ | cut -d ' ' -f 3 | sed 's/+//')
-    echo "$CHANGED_LINES"
-    for number in $CHANGED_LINES
-    do
-        LINE=$(sed "$number!d" che-theia-plugins.yaml | xargs)
-        echo --- LINE --- 
-        echo "$LINE"
-        if [[ $LINE == *"revision:"* ]]; then
-            break
-        fi
-        exit 0
-    done
-    EXTENSION_REVISION=$(echo "$LINE" | cut -d ' ' -f 2)
-    EXTENSION_REPO=$(yq -r --arg EXTENSION_REVISION "$EXTENSION_REVISION" '[.plugins[] | select(.repository.revision == $EXTENSION_REVISION )] | .[0] | .repository.url' che-theia-plugins.yaml)
-    # export EXTENSION_REPO
-    # export EXTENSION_REVISION
-
-    echo --- REPOSITORY ---
-    echo Extension repo is "$EXTENSION_REPO"
-    echo --- REVISION ---
-    echo Extension revision is "$EXTENSION_REVISION"
-    echo "export EXTENSION_REPO=${EXTENSION_REPO}" > ~/.bashrc
-}
-
-getExtensionRepoAndRevision
-source ~/.bashrc
+CHANGED_LINES=$(git diff -U0 HEAD "$(git merge-base HEAD origin/master)" che-theia-plugins.yaml | grep @@ | cut -d ' ' -f 3 | sed 's/+//')
+echo "$CHANGED_LINES"
+for number in $CHANGED_LINES
+do
+    LINE=$(sed "$number!d" che-theia-plugins.yaml | xargs)
+    echo --- LINE --- 
+    echo "$LINE"
+    if [[ $LINE == *"revision:"* ]]; then
+        break
+    fi
+    exit 0
+done
+EXTENSION_REVISION=$(echo "$LINE" | cut -d ' ' -f 2)
+EXTENSION_REPO=$(yq -r --arg EXTENSION_REVISION "$EXTENSION_REVISION" '[.plugins[] | select(.repository.revision == $EXTENSION_REVISION )] | .[0] | .repository.url' che-theia-plugins.yaml)
+# export EXTENSION_REPO
+# export EXTENSION_REVISION
+echo --- REPOSITORY ---
+echo Extension repo is "$EXTENSION_REPO"
+echo --- REVISION ---
+echo Extension revision is "$EXTENSION_REVISION"
+echo "export EXTENSION_REPO=${EXTENSION_REPO}" > ~/.bashrc
