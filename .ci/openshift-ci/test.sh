6bd177ff73f4765b930e5756818f606f34b92099
Ihor Okhrimenko <iokhrime@redhat.com>
2021-03-18 12:20:19 +0200

    Update test
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/openshift-ci/test.sh
+++ .ci/openshift-ci/test.sh
@@ -20,6 +20,8 @@ set -o pipefail
 # error on unset variables
 set -u
 
+export TEST_POD_NAMESPACE="devworkspace-project"
+
 # OPERATOR_REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # echo $OPERATOR_REPO
 # source "${OPERATOR_REPO}"/common.sh
@@ -33,13 +35,6 @@ deployChe() {
 }
 
 runTest() {
-  echo "================================>>>>>>>>>>>>>>>>>>>>>>>>>>"
-  env
-  echo "TEST_SUITE $TEST_SUITE"
-  echo "NODE_TLS_REJECT_UNAUTHORIZED $NODE_TLS_REJECT_UNAUTHORIZED"
-  echo "TS_SELENIUM_LOG_LEVEL $TS_SELENIUM_LOG_LEVEL"
-  echo "================================>>>>>>>>>>>>>>>>>>>>>>>>>>"
-
   deployChe
 #   waitDevWorkspaceControllerStarted
 
@@ -60,24 +55,24 @@ runTest() {
 
 
   # patch pod.yaml 
-  wget https://raw.githubusercontent.com/Ohrimenko1988/che/openshiftCi/.ci/openshift-ci/happy-path-pod.yaml
+  wget https://gist.githubusercontent.com/Ohrimenko1988/cf2ca8040f2e9fabaa4557c87c89181e/raw/454041a72086d8dc7acfe3be1c753556b2ed55dc/happy-path-pod.yaml
  
  
-#   ECLIPSE_CHE_URL=http://$(oc get route -n "${NAMESPACE}" che -o jsonpath='{.status.ingress[0].host}')
+  ECLIPSE_CHE_URL=http://$(oc get route -n "eclipse-che" che -o jsonpath='{.status.ingress[0].host}')
 #   TS_SELENIUM_DEVWORKSPACE_URL="https://$(oc get route -n "${NAMESPACE}" | grep theia/ | awk '{print $2}')/theia/"
-#   sed -i "s@CHE_URL@${ECLIPSE_CHE_URL}@g" happy-path-pod.yaml
+  sed -i "s@CHE_URL@${ECLIPSE_CHE_URL}@g" happy-path-pod.yaml
 #   sed -i "s@WORKSPACE_ROUTE@${TS_SELENIUM_DEVWORKSPACE_URL}@g" happy-path-pod.yaml
-#   cat happy-path-pod.yaml
+  cat happy-path-pod.yaml
 
 
 
-  oc create namespace devworkspace-project
+  oc create namespace $TEST_POD_NAMESPACE
 
   oc apply -f happy-path-pod.yaml
   # wait for the pod to start
   while true; do
     sleep 3
-    PHASE=$(oc get pod -n ${NAMESPACE} happy-path-che \
+    PHASE=$(oc get pod -n ${TEST_POD_NAMESPACE} happy-path-che \
         --template='{{ .status.phase }}')
     if [[ ${PHASE} == "Running" ]]; then
         break
@@ -85,15 +80,15 @@ runTest() {
   done
 
   # wait for the test to finish
-  oc logs -n ${NAMESPACE} happy-path-che -c happy-path-test -f
+  oc logs -n ${TEST_POD_NAMESPACE} happy-path-che -c happy-path-test -f
 
   # just to sleep
   sleep 3
 
   # download the test results
   mkdir -p tmp/
-  oc rsync -n ${NAMESPACE} happy-path-che:/tmp/e2e/report/ tmp/ -c download-reports
-  oc exec -n ${NAMESPACE} happy-path-che -c download-reports -- touch /tmp/done
+  oc rsync -n ${TEST_POD_NAMESPACE} happy-path-che:/tmp/e2e/report/ tmp/ -c download-reports
+  oc exec -n ${TEST_POD_NAMESPACE} happy-path-che -c download-reports -- touch /tmp/done
 }
 
 # initDefaults
