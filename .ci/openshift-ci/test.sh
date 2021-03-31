c59db57a97bc6441041e5a0b4b61cb21531000a5
Ihor Okhrimenko <iokhrime@redhat.com>
2021-03-31 16:36:28 +0300

    Update test
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/openshift-ci/test.sh
+++ .ci/openshift-ci/test.sh
@@ -26,15 +26,6 @@ export PLUGIN_REGISTRY_IMAGE=${CHE_PLUGIN_REGISTRY}
 export HAPPY_PATH_POD_NAME="happy-path-che"
 export ARTIFACTS_DIR=${ARTIFACT_DIR:-"/tmp/artifacts-che"}
 
-
-# OPERATOR_REPO="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-# echo $OPERATOR_REPO
-# source "${OPERATOR_REPO}"/common.sh
-# source "${OPERATOR_REPO}"/oauth-provision.sh
-
-# Stop execution on any error
-# trap "catchFinish" EXIT SIGINT
-
 createCustomResourcesFile() {
   cat > custom-resources.yaml <<-END
 spec:
@@ -44,6 +35,9 @@ spec:
     pluginRegistryImage: ${PLUGIN_REGISTRY_IMAGE}
     pluginRegistryPullPolicy: IfNotPresent
     customCheProperties:
+      CHE_LIMITS_USER_WORKSPACES_RUN_COUNT: '-1'
+      CHE_LIMITS_WORKSPACE_IDLE_TIMEOUT: '900000'
+      CHE_INFRA_KUBERNETES_WORKSPACE__UNRECOVERABLE__EVENTS: 'Failed Scheduling,Failed to pull image'
       CHE_WORKSPACE_SIDECAR_IMAGE__PULL__POLICY: IfNotPresent
       CHE_WORKSPACE_PLUGIN__BROKER_PULL__POLICY: IfNotPresent
       CHE_INFRA_KUBERNETES_PVC_JOBS_IMAGE_PULL__POLICY: IfNotPresent
@@ -57,53 +51,21 @@ deployChe() {
   chectl server:deploy  --che-operator-cr-patch-yaml=custom-resources.yaml --telemetry=off --workspace-engine=dev-workspace --platform=openshift --installer=operator --batch
 }
 
-runTest() {
-  createCustomResourcesFile
-  deployChe
-#   waitDevWorkspaceControllerStarted
-
-#   # wait for 2 min for devworkspace-controller ready to work.
-#   sleep 120
-#   createWorkspaceDevWorkspaceController
-#   waitWorkspaceStartedDevWorkspaceController 
-#   sleep 120
-#   oc get pods -n ${NAMESPACE}
-#   oc get routes -n ${NAMESPACE}
-
-#   wsname=$(oc get pods  | grep workspace | awk '{print $1}')
-#   oc logs $wsname -c theia-ide
-#   oc logs $wsname -c terminal
-#   oc get events -n ${NAMESPACE}
-
-
-
-
-  # patch pod.yaml
-  echo "===>>>  Show where script launched"
-  echo "---------------------------------------"
-  pwd
-
-  echo "---------------------------------------"
-  ls -al
-
-  echo "---------------------------------------"
-  # wget https://gist.githubusercontent.com/Ohrimenko1988/cf2ca8040f2e9fabaa4557c87c89181e/raw/a77180ae40e630acc204076689397d5ebbf604ce/happy-path-pod.yaml
-  
+patchTestPodConfig(){
+  # obtain the basic "happy-path" pod config
   cat .ci/openshift-ci/happy-path-pod.yaml > happy-path-pod.yaml
 
-
+  # Patch the basic "happy-path" pod config
   ECLIPSE_CHE_URL=http://$(oc get route -n "eclipse-che" che -o jsonpath='{.status.ingress[0].host}')
-#   TS_SELENIUM_DEVWORKSPACE_URL="https://$(oc get route -n "${NAMESPACE}" | grep theia/ | awk '{print $2}')/theia/"
   sed -i "s@CHE_URL@${ECLIPSE_CHE_URL}@g" happy-path-pod.yaml
-
-#   sed -i "s@WORKSPACE_ROUTE@${TS_SELENIUM_DEVWORKSPACE_URL}@g" happy-path-pod.yaml
   cat happy-path-pod.yaml
+}
 
-
-
+runTest() {
+  # Create the test pod
   oc create namespace $TEST_POD_NAMESPACE
-
   oc apply -f happy-path-pod.yaml
+
   # wait for the pod to start
   while true; do
     sleep 3
@@ -121,13 +83,6 @@ runTest() {
   sleep 3
 
   # download the test results
-  # mkdir -p tmp/
-  # oc rsync -n ${TEST_POD_NAMESPACE} happy-path-che:/tmp/e2e/report/ ${ARTIFACTS_DIR} -c download-reports
-  # oc exec -n ${TEST_POD_NAMESPACE} happy-path-che -c download-reports -- touch ${ARTIFACTS_DIR}/done
-
-
-
-  # download the test results
   mkdir -p /tmp/e2e
   oc rsync -n ${TEST_POD_NAMESPACE} ${HAPPY_PATH_POD_NAME}:/tmp/e2e/report/ /tmp/e2e -c download-reports
   oc exec -n ${TEST_POD_NAMESPACE} ${HAPPY_PATH_POD_NAME} -c download-reports -- touch /tmp/done
@@ -136,6 +91,7 @@ runTest() {
   cp -r /tmp/e2e ${ARTIFACTS_DIR}
 }
 
-# initDefaults
-# installYq
+createCustomResourcesFile
+deployChe
+patchTestPodConfig
 runTest
