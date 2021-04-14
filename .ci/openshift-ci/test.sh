5975c7922ad537bb5ea1a015e980e561d9703664
Ihor Okhrimenko <iokhrime@redhat.com>
2021-04-14 15:03:33 +0300

    Apply shellcheck suggestions
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/openshift-ci/test.sh
+++ .ci/openshift-ci/test.sh
@@ -34,8 +34,8 @@ provisionOpenShiftOAuthUser() {
 
   echo -e "[INFO] Waiting for htpasswd auth to be working up to 5 minutes"
   CURRENT_TIME=$(date +%s)
-  ENDTIME=$(($CURRENT_TIME + 300))
-  while [ $(date +%s) -lt $ENDTIME ]; do
+  ENDTIME=$((CURRENT_TIME + 300))
+  while [ "$(date +%s)" -lt $ENDTIME ]; do
       if oc login -u user -p user --insecure-skip-tls-verify=false; then
           break
       fi
