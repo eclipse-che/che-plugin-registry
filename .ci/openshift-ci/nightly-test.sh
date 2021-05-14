22261e24276dd6a71259837fe759275b13014eb9
Ihor Okhrimenko <iokhrime@redhat.com>
2021-05-14 11:18:10 +0300

    Add nightly tests run
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/openshift-ci/nightly-test.sh
+++ .ci/openshift-ci/nightly-test.sh
@@ -21,8 +21,17 @@ set -o pipefail
 source ./common-functions.sh
 
 runTests() {
+  runTest "InstallPluginUsingUI" "install-plugin-test"
   runTest "JavaPlugin" "java11-plugin-test"
+  runTest "PhpPlugin" "php-plugin-test"
+  runTest "PythonPluginTest" "python-plugin-test"
   runTest "TypescriptPlugin" "typescript-debug-plugins"
+  runTest "VscodeKubernetesPlugin" "nodejs-24lop"
+  runTest "VscodeShellcheckPlugin" "nodejs-zmecm"
+  runTest "VscodeValePlugin" "che-docs-test"
+  runTest "VscodeXmlPlugin" "xml-plugin-test"
+  runTest "VscodeYamlPlugin" "nodejs-zmecm"
+
 }
 
 setupTestEnvironment
