d36f189df5e8d085d38f1503071a34acdd3c4a7f
Vitaliy Gulyy <vgulyy@redhat.com>
2021-05-28 00:40:34 +0300

    Icon path must be relative to plugin registry root folder
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- tools/build/tests/meta-yaml/meta-yaml-writer.spec.ts
+++ tools/build/tests/meta-yaml/meta-yaml-writer.spec.ts
@@ -103,7 +103,7 @@ type: VS Code extension
 displayName: display-name
 title: my-title
 description: my-description
-icon: /v3/images/my-publisher-my-name-icon.png
+icon: /images/my-publisher-my-name-icon.png
 category: Programming Languages
 repository: 'http://fake-repository'
 firstPublicationDate: '2019-01-01'
@@ -161,7 +161,7 @@ type: VS Code extension
 displayName: display-name
 title: my-title
 description: my-description
-icon: /v3/images/eclipse-che-logo.png
+icon: /images/eclipse-che-logo.png
 category: Programming Languages
 repository: 'http://fake-repository'
 firstPublicationDate: '2019-01-01'
@@ -239,7 +239,7 @@ type: VS Code extension
 displayName: display-name
 title: my-title
 description: my-description
-icon: /v3/images/eclipse-che-logo.png
+icon: /images/eclipse-che-logo.png
 category: Programming Languages
 repository: 'http://fake-repository'
 firstPublicationDate: '2019-01-01'
@@ -319,7 +319,7 @@ type: VS Code extension
 displayName: display-name
 title: my-title
 description: my-description
-icon: /v3/images/eclipse-che-logo.png
+icon: /images/eclipse-che-logo.png
 category: Programming Languages
 repository: 'http://fake-repository'
 firstPublicationDate: '2019-01-01'
@@ -379,7 +379,7 @@ type: VS Code extension
 displayName: display-name
 title: my-title
 description: my-description
-icon: /v3/images/eclipse-che-logo.png
+icon: /images/eclipse-che-logo.png
 category: Programming Languages
 repository: 'http://fake-repository'
 firstPublicationDate: '2019-01-01'
