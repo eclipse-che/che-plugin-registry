947c7aa0812ef12c2cb821ceb06e82af3410f467
Mykhailo Kuznietsov <mkuznets@redhat.com>
2021-10-05 15:09:52 +0300

    fix: fix messages in the release script (#1063)
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -83,9 +83,9 @@ commitChangeOrCreatePR()
     PR_BRANCH="$3"
 
     if [[ ${PR_BRANCH} == *"add"* ]]; then
-      COMMIT_MSG="[release] Add ${aVERSION} plugins in ${aBRANCH}"
+      COMMIT_MSG="chore: Add ${aVERSION} plugins in ${aBRANCH}"
     else 
-      COMMIT_MSG="[release] Bump to ${aVERSION} in ${aBRANCH}"
+      COMMIT_MSG="chore: Bump to ${aVERSION} in ${aBRANCH}"
     fi
 
     # commit change into branch
