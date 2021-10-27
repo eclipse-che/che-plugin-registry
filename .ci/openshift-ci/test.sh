d9be6cadf33b677222f4e5b01f7445bbb66244a4
Ihor Okhrimenko <iokhrime@redhat.com>
2021-10-27 14:28:21 +0300

    Update
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/openshift-ci/test.sh
+++ .ci/openshift-ci/test.sh
@@ -24,12 +24,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}"/common-functions.sh
 
 runTests() {
-  runTest "GitHubPullRequestPlugin" "github-pr-plugin"
-
-  # runTest "VscodeYamlPlugin" "nodejs-zmecm"
-  
-  # Disabled because of known bug https://github.com/eclipse/che/issues/20075
-  # runTest "TypescriptPlugin" "typescript-debug-plugins"
+  runTest "VscodeYamlPlugin" "nodejs-zmecm"
+  runTest "TypescriptPlugin" "typescript-debug-plugins"
 }
 
 setupTestEnvironment
