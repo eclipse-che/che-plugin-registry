0248cad3e8873eb4300f58a60df69d885c5357bc
Florent Benoit <fbenoit@redhat.com>
2022-07-01 15:41:25 +0200

    feat: Allow to use OpenVsx in plug-in registry
    Change-Id: I6b42aa61a6d6b8077441fab270d6950954e29698
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- build/dockerfiles/entrypoint.sh
+++ build/dockerfiles/entrypoint.sh
@@ -47,7 +47,29 @@ function run_main() {
 
     update_extension_vsx_references
 
-    exec "${@}"
+    # Add current (arbitrary) user to /etc/passwd and /etc/group
+    if ! whoami &> /dev/null; then
+        if [ -w /etc/passwd ]; then
+            echo "${USER_NAME:-postgres}:x:$(id -u):0:${USER_NAME:-postgres} user:${HOME}:/sbin/nologin" >> /etc/passwd
+        fi
+    fi
+
+    # if /var/lib/pgsql exists
+    if [ -d /var/lib/pgsql ]; then
+      # change permissions
+      cp -r /var/lib/pgsql/14/data/old /var/lib/pgsql/14/data/database
+      rm -rf /var/lib/pgsql/14/data/old
+
+      # start postgres and openvsx
+      ./start-services.sh
+    fi
+
+    # start httpd
+    if [[ -x /usr/sbin/httpd ]]; then
+      /usr/sbin/httpd -D FOREGROUND
+    elif [[ -x /usr/bin/run-httpd ]]; then
+      /usr/bin/run-httpd
+    fi
 
 }
 
