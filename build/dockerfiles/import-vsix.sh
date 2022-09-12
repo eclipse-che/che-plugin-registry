d07f8befeb11fb60d8f00dfcb0200ec0bdfef811
Valerii Svydenko <vsvydenk@redhat.com>
2022-09-12 13:55:58 +0300

    chore: update ovsx; disable embedded elasticsearch
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>

--- build/dockerfiles/import-vsix.sh
+++ build/dockerfiles/import-vsix.sh
@@ -12,7 +12,7 @@ export PATH=/tmp/opt/nodejs/bin:$PATH
 
 
 # install the cli
-npm install -g ovsx@0.2.1
+npm install -g ovsx@0.5.0
 
 # insert user
 psql -c "INSERT INTO user_data (id, login_name) VALUES (1001, 'eclipse-che');"
