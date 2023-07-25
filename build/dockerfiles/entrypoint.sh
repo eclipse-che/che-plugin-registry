292587bad53b056537737d69f871c85653bfef58
Valeriy Svydenko <vsvydenk@redhat.com>
2023-07-25 15:15:29 +0300

    chore:update openvsx image to use more recent version (#1736)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- build/dockerfiles/entrypoint.sh
+++ build/dockerfiles/entrypoint.sh
@@ -58,8 +58,8 @@ function run_main() {
     # start only if wanted
     if [ "${START_OPENVSX}" == "true" ]; then
       # change permissions
-      cp -r /var/lib/pgsql/14/data/old /var/lib/pgsql/14/data/database
-      rm -rf /var/lib/pgsql/14/data/old
+      cp -r /var/lib/pgsql/15/data/old /var/lib/pgsql/15/data/database
+      rm -rf /var/lib/pgsql/15/data/old
 
       # start postgres and openvsx
       ./start-services.sh
