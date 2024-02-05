374b7c826c93f19732af181829e85b40468521d7
Valeriy Svydenko <vsvydenk@redhat.com>
2024-02-05 12:45:20 +0200

    chore: add more logs to the publish script
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- build/dev/publish-plugin-registry-to-gh-pages.sh
+++ build/dev/publish-plugin-registry-to-gh-pages.sh
@@ -13,6 +13,8 @@ set -e
 
 DEFAULT_BUILD_DIR="/projects/che-plugin-registry/output"
 BUILD_DIR=${BUILD_DIR:-$DEFAULT_BUILD_DIR}
+echo "BUILD_DIR: $BUILD_DIR"
+ls -l "$BUILD_DIR"
 
 DEFAULT_GH_PAGES_DIR="/projects/gh-pages"
 GH_PAGES_DIR=${GH_PAGES_DIR:-$DEFAULT_GH_PAGES_DIR}
@@ -25,21 +27,25 @@ GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-$DEFAULT_GITHUB_REPOSITORY}
 
 DEFAULT_VERSION_DIR="next"
 VERSION_DIR=${VERSION_DIR:-$DEFAULT_VERSION_DIR}
+echo "VERSION_DIR: $VERSION_DIR"
 
 rm "$GH_PAGES_DIR" -rf;
 mkdir "$GH_PAGES_DIR"
 cd "$GH_PAGES_DIR"
+echo "GH Pages dir: $(pwd)"
+ls -l "$GH_PAGES_DIR"
+
 rm -rf che-plugin-registry
 git clone -b gh-pages "https://github.com/$GITHUB_REPOSITORY.git" "$GITHUB_REPO_NAME"
 cd "$GITHUB_REPO_NAME"
 [ -d "$VERSION_DIR" ] && git rm -r "$VERSION_DIR"
 cp -rf "$BUILD_DIR" "$VERSION_DIR"
-# Make meta.yaml as index
+# Make devfile.yaml as index
 while IFS= read -r -d '' file
 do
   PARENT_DIR=$(dirname "$file");
-  cp "${PARENT_DIR}"/meta.yaml "${PARENT_DIR}"/index.json;
-done <   <(find . -name 'meta.yaml' -type f -print0)
+  cp "${PARENT_DIR}"/devfile.yaml "${PARENT_DIR}"/index.json;
+done <   <(find . -name 'devfile.yaml' -type f -print0)
 git add "$VERSION_DIR"
 git config user.email "che-bot@eclipse.org"
 git config user.name "CHE Bot"
