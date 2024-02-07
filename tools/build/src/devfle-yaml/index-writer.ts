119a0feb64204d7c225f2ad13267da4b5b7e624d
Valerii Svydenko <vsvydenk@redhat.com>
2024-02-07 18:26:31 +0200

    feat: provide icons for the editors (#1885)
    * feat: provide icons for the editors
    
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- tools/build/src/devfle-yaml/index-writer.ts
+++ tools/build/src/devfle-yaml/index-writer.ts
@@ -13,8 +13,7 @@ import * as path from 'path';
 
 import { inject, injectable, named } from 'inversify';
 
-import { V222Devfile } from '@devfile/api';
-import { DevfileYamlWriter } from './devfile-yaml-writer';
+import { V222Devfile, V222DevfileMetadata } from '@devfile/api';
 
 /**
  * Write in a file named index.json, all the plugins that can be found.
@@ -25,10 +24,8 @@ export class IndexWriter {
   @named('OUTPUT_ROOT_DIRECTORY')
   private outputRootDirectory: string;
 
-  @inject(DevfileYamlWriter)
-  private devfileYamlWriter: DevfileYamlWriter;
-
   private CHE_EDITOR_TYPE = 'Che Editor';
+  private DEFAULT_ICON = '/images/default.svg';
 
   async write(editors: V222Devfile[]): Promise<void> {
     const v3PluginsFolder = path.resolve(this.outputRootDirectory, 'v3', 'plugins');
@@ -36,7 +33,9 @@ export class IndexWriter {
     const externalImagesFile = path.join(v3PluginsFolder, 'index.json');
 
     const indexValues = editors.map(editor => {
-      let id = this.devfileYamlWriter.verifyEditorId(editor);
+      const metadata = this.getMetadata(editor);
+
+      let id = this.getId(metadata);
       const splitIds = id.split('/');
       const publisher = splitIds[0];
       const name = splitIds[1];
@@ -47,10 +46,14 @@ export class IndexWriter {
       }
       id = publisher + '/' + name + '/' + version;
 
+      const icon = this.getIcon(metadata);
+      const iconPathPrefix = '/v3';
+      const iconPath = iconPathPrefix + icon;
+
       return {
         id: id,
-        description: editor.metadata?.description,
-        displayName: editor.metadata?.displayName,
+        description: metadata.description,
+        displayName: metadata.displayName,
         links: {
           devfile: `/v3/plugins/${id}/devfile.yaml`,
         },
@@ -58,9 +61,39 @@ export class IndexWriter {
         publisher: publisher,
         type: this.CHE_EDITOR_TYPE,
         version: version,
+        icon: iconPath,
       };
     });
     indexValues.sort((pluginA, pluginB) => pluginA.id.localeCompare(pluginB.id));
     await fs.writeFile(externalImagesFile, JSON.stringify(indexValues, undefined, 2));
+  }
+
+  getMetadata(editor: V222Devfile): V222DevfileMetadata {
+    const metadata = editor.metadata;
+    if (!metadata) {
+      throw new Error(`The metadata of ${JSON.stringify(editor)} is not defined`);
+    }
+    return metadata;
+  }
+
+  getIcon(metadata: V222DevfileMetadata): string {
+    let icon = metadata.icon;
+    if (!icon) {
+      // If the icon is not defined, we use the default one
+      icon = this.DEFAULT_ICON;
+    }
+    return icon;
+  }
+
+  getId(metadata: V222DevfileMetadata): string {
+    const id = metadata.name;
+    if (!id) {
+      throw new Error(`The id of ${JSON.stringify(metadata)} is not defined`);
+    }
+    const values = id.split('/');
+    if (values.length !== 3) {
+      throw new Error(`The id for ${id} is not composed of 3 parts separated by / like <1>/<2>/<3>`);
+    }
+    return id;
   }
 }
